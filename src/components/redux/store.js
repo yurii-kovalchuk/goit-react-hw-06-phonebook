653ebc5b8afa4c3fb9a11e8cf16d2d800c791a5d
yurii-kovalchuk <sogdrums@gmail.com>
2023-02-21 16:46:21 +0200

    fix bug with middleware in store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,13 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { persistContactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
-import { persistStore } from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
     contacts: persistContactsReducer,
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
